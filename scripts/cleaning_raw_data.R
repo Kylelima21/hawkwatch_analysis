bbea3b9cc1fa068cae2f52570bfb40397c41b56b
Kyle Lima <kylelima@Kyles-MBP.lan>
2023-01-20 14:38:34 -0500

    inital setup and organization

---
+++ scripts/cleaning_raw_data.R
@@ -0,0 +1,17 @@
+#Cadillac Hawkwatch data cleaning
+#Schoodic Institute at Acadia National Park
+#Kyle Lima, 2023
+
+
+#------------------------------------------------#
+####           Packages Required              ####
+#------------------------------------------------#
+library(tidyverse)
+
+
+
+#------------------------------------------------#
+####             Read and Clean               ####
+#------------------------------------------------#
+
+raw <- readxl::read_excel(path = "data/Cadillac Mt., Acadia National Park.xls", skip = 2)
